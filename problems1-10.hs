728b320dd5a9d758d4fdcccbc0e6f3b06ec9bbba
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 14:37:51 +0100

    Done Problem 8

--- problems1-10.hs
+++ problems1-10.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 problem1 = sum [x| x<-[1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
 
 problem2 = sum [x | x <-takeWhile (<4000000) $ map fib [1..], even x]
@@ -12,5 +14,15 @@ problem4 = maximum $ [a*b| a<-[100..999], b<-[a..999], (reverse $ show (a*b) ) =
 problem5 = foldr1 lcm [1..20]
 
 problem6 = (sum [1..100])^2 - sum ( map (^2) [1..100] )
+
+problem8 = do
+    inFile <- readFile "p8.dat"
+    let number = read inFile :: Integer
+    let productsList = findProducts (show number)
+    print $ maximum productsList
+    where
+        findProducts :: String -> [Int]
+        findProducts [] = []
+        findProducts x = [product $ map digitToInt $ take 5 x ] ++ (findProducts (tail x))
 
 problem9 = product $ head [ [a,b,c] | c<-[1..1000], b<-[1..(1000-c-1)], a<-[1000-c-b], a^2 + b^2 == c^2 ]
