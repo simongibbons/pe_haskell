60a65dba32ba4bff1f6ab2f08dbd3641b2df04ce
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 09:23:53 +0100

    Added Problem 9

--- problems1-10.hs
+++ problems1-10.hs
@@ -8,3 +8,5 @@ problem2 = sum [x | x <-takeWhile (<4000000) $ map fib [1..], even x]
           fib_base n = fib (n-2) + fib (n-1)
 
 problem6 = (sum [1..100])^2 - sum ( map (^2) [1..100] )
+
+problem9 = product $ head [ [a,b,c] | c<-[1..1000], b<-[1..(1000-c-1)], a<-[1000-c-b], a^2 + b^2 == c^2 ]
