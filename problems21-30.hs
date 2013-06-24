dd1345e8070bc56193c55e11dc21cba08f2adfbb
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 12:51:28 +0100

    Added problem 40

--- problems21-30.hs
+++ problems21-30.hs
@@ -4,3 +4,8 @@ problem25 = head [x+1 | x<-[1..], (length $ show $ fib $ x) == 1000 ]
           fib_base 0 = 1
           fib_base 1 = 1
           fib_base n = fib (n-2) + fib (n-1)
+
+problem30 = sum $ [x | x<-[2..(6*9^5)], isFifthPowerSum x ]
+    where isFifthPowerSum x = x == (sum $ map (fifthPower . digitToInt) (show x))
+          fifthPower n = fifthPower_list !! n
+          fifthPower_list = map (^5) [0..9]
--- problems31-40.hs
+++ problems31-40.hs
@@ -1,11 +1,10 @@
 import Data.Char (digitToInt)
 
-problem30 = sum $ [x | x<-[2..(6*9^5)], isFifthPowerSum x ]
-    where isFifthPowerSum x = x == (sum $ map (fifthPower . digitToInt) (show x))
-          fifthPower n = fifthPower_list !! n
-          fifthPower_list = map (^5) [0..9]
-
 problem34 = sum [x | x<-[3..100000], isFacOfDig x]
     where isFacOfDig x = x == (sum $ map (fac . digitToInt) (show x))
           fac n = fac_list !! n
           fac_list = [1] ++ [ product [1..y] | y<-[1..9]]
+
+
+problem40 = product [ digitToInt (champVals !! (10^n)) | n<-[0..6] ]
+    where champVals = foldr1 (++) (map show [0..])
