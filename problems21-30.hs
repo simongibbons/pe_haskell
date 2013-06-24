2f749bdd73f3a9a3288b20958009a79dcfb7ca48
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 12:59:40 +0100

    Done problem 29

--- problems21-30.hs
+++ problems21-30.hs
@@ -1,9 +1,14 @@
+import Data.List
+import Data.Char
+
 problem25 = head [x+1 | x<-[1..], (length $ show $ fib $ x) == 1000 ]
     where fib :: Int -> Integer
           fib = (map fib_base [0 ..] !!)
           fib_base 0 = 1
           fib_base 1 = 1
           fib_base n = fib (n-2) + fib (n-1)
+
+problem29 = length $ nub $ [a^b| a<-[2..100], b<-[2..100] ]
 
 problem30 = sum $ [x | x<-[2..(6*9^5)], isFifthPowerSum x ]
     where isFifthPowerSum x = x == (sum $ map (fifthPower . digitToInt) (show x))
