938da93ff2c143d0a14f0cdd1681e40d9c50c158
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 12:38:11 +0100

    problems11-20.hs

--- problems11-20.hs
+++ problems11-20.hs
@@ -1,4 +1,13 @@
 import Data.Char
+import Data.List
+import Data.Ord (comparing)
+
+problem14 = maximumBy (comparing snd) [(x, chainLength x) | x<-[1..1000000] ]
+    where chainLength :: Int -> Int
+          chainLength 1 = 1
+          chainLength n
+                    | even n = 1 + chainLength ( n `div` 2)
+                    | odd n  = 1 + chainLength (3*n + 1)
 
 problem15 = choose 40 20
     where choose n k = product [(k+1)..n] `div` product [1..k]
