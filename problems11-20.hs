a916a18058469851f443cebc0cd252089061a10e
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 09:12:23 +0100

    Added problem 20

--- problems11-20.hs
+++ problems11-20.hs
@@ -2,3 +2,7 @@ import Data.Char
 
 problem16 = power2sum 1000
     where power2sum n = sum (map digitToInt (show (2^n)))
+
+problem20 = facSum 100
+    where facSum n = sum $ map digitToInt $ show (fac n)
+          fac n    = product [1..n]
