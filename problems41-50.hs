e8f17f26f2d2dc4aca32c68d560b406effd14e86
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 15:17:01 +0100

    Added Problem42

--- problems41-50.hs
+++ problems41-50.hs
@@ -1,1 +1,12 @@
+import Data.Char
+
+problem42 = do
+    fileIn <- readFile "p42.dat"
+    let wordSums = map sumWord (read fileIn :: [String])
+    let triNos = triangularToN $ maximum wordSums
+    print $ length $ filter (\x -> x `elem` triNos) wordSums
+    where
+        sumWord word = sum $ map (\x -> (ord x) - 64) word
+        triangularToN n = takeWhile (<n) $ map (\x -> x*(x+1) `div` 2) [1..]
+
 problem48 = ( sum $ [x^x `mod` (10^10) | x<-[1..1000] ] ) `mod` (10^10)
