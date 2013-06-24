0940ddbde924d1c670b72cc63eed17baded5dd5e
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 10:20:15 +0100

    Added Problem 15

--- problems11-20.hs
+++ problems11-20.hs
@@ -1,5 +1,8 @@
 import Data.Char
 
+problem15 = choose 40 20
+    where choose n k = product [(k+1)..n] `div` product [1..k]
+
 problem16 = power2sum 1000
     where power2sum n = sum $ map digitToInt $ show (2^n)
 
