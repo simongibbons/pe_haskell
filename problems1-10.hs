5f042f616a4dd5719b6e5e807af5db395eaa5d6c
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 12:14:25 +0100

    Added Problem 5

--- problems1-10.hs
+++ problems1-10.hs
@@ -9,6 +9,8 @@ problem2 = sum [x | x <-takeWhile (<4000000) $ map fib [1..], even x]
 
 problem4 = maximum $ [a*b| a<-[100..999], b<-[a..999], (reverse $ show (a*b) ) == (show (a*b))]
 
+problem5 = foldr1 lcm [1..20]
+
 problem6 = (sum [1..100])^2 - sum ( map (^2) [1..100] )
 
 problem9 = product $ head [ [a,b,c] | c<-[1..1000], b<-[1..(1000-c-1)], a<-[1000-c-b], a^2 + b^2 == c^2 ]
