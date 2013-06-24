577d07bb3297f13abdcae3a5bcfdc1029e990b60
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 10:12:08 +0100

    Added problem 4

--- problems1-10.hs
+++ problems1-10.hs
@@ -7,6 +7,8 @@ problem2 = sum [x | x <-takeWhile (<4000000) $ map fib [1..], even x]
           fib_base 1 = 1
           fib_base n = fib (n-2) + fib (n-1)
 
+problem4 = maximum $ [a*b| a<-[100..999], b<-[100..999], (reverse $ show (a*b) ) == (show (a*b))]
+
 problem6 = (sum [1..100])^2 - sum ( map (^2) [1..100] )
 
 problem9 = product $ head [ [a,b,c] | c<-[1..1000], b<-[1..(1000-c-1)], a<-[1000-c-b], a^2 + b^2 == c^2 ]
