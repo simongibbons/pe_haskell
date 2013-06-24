53ef53866beee86e2fd7b8e7dbd91722b1380b21
Simon Gibbons <simongibbons@gmail.com>
2013-06-24 14:18:51 +0100

    Done Problem 99

---
+++ problems91-100.hs
@@ -0,0 +1,14 @@
+import Data.Ord (comparing)
+import Data.List
+
+problem99 = do
+    inList <- readFile "p99.dat"
+    let baseExpList = createIntTuples inList
+    --Find The Maximum Value
+    let maxval = fst $ maximumBy (comparing snd) [(x, ((snd x) * log(fst x)) ) | x<-baseExpList]
+    print $ findIndex maxval baseExpList + 1
+        where
+            createIntTuples :: String -> [(Double, Double)]
+            createIntTuples input = map read $ lines input
+            findIndex val x = head ( filter ( (==val) . (x!!) ) [0..((length x) - 1)] )
+ 
