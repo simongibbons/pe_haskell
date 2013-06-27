551d1782707a7607114b3debadf48f0d15859d04
Simon Gibbons <simongibbons@gmail.com>
2013-06-27 12:07:51 +0100

    Done P92

--- problems91-100.hs
+++ problems91-100.hs
@@ -1,5 +1,20 @@
 import Data.Ord (comparing)
 import Data.List
+import Data.Char
+
+--Should Memoize This to improve the speed.
+problem92 = length [1 | x<-[1..9999999], (endNo x) == 89 ]
+    where
+        endNo :: Int -> Int
+        endNo 89 = 89
+        endNo 1 = 1
+        endNo n = endNo $ squareDig n
+
+        squareDig :: Int -> Int
+        squareDig n = sum $ map (^2) $ digits n
+
+        digits = map (`mod` 10) . reverse . takeWhile (> 0) . iterate (`div` 10)
+
 
 problem99 = do
     inList <- readFile "data/p99.dat"
