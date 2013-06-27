059bc667cf56e4920c7be54ff0dcc2a6aaf575ed
Simon Gibbons <simongibbons@gmail.com>
2013-06-27 15:07:53 +0100

    Done P17

--- problems11-20.hs
+++ problems11-20.hs
@@ -25,6 +25,23 @@ problem15 = choose 40 20
 problem16 = power2sum 1000
     where power2sum n = sum $ map digitToInt $ show (2^n)
 
+problem17 = sum $ map ( length . letters ) [1..1000]
+    where
+        ones = ["one","two","three","four","five","six","seven","eight",
+                "nine","ten","eleven","twelve","thirteen","fourteen","fifteen",
+                "sixteen","seventeen","eighteen", "nineteen"]
+        tys  = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
+
+        letters x
+            | x == 0        = []
+            | x < 20        = ones !! (x-1)
+            | x < 100       = tys !! ( (x `div` 10) - 2 ) ++  letters (x - (x `div` 10) * 10)
+            | x < 1000 && (x `mod` 100) == 0 
+                            = ones !! ( (x `div` 100 ) -1 ) ++ "hundred"
+            | x > 100 && x <= 999
+                            = ones !! ( (x `div` 100 ) -1 ) ++ "hundredand" ++ letters (x - (x `div` 100) * 100)
+            | x == 1000     = "onethousand"
+
 problem18 = do
     inFile <- readFile "data/p18.dat"
     print $ maxPathSum (read inFile :: [[Int]])
