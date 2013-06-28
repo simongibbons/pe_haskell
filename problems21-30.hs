fa0737a9a9f7f3ab5d6055ce5fa9cde3ba7d6f35
Simon Gibbons <simongibbons@gmail.com>
2013-06-28 11:16:01 +0100

    Done Problem 27

--- problems21-30.hs
+++ problems21-30.hs
@@ -1,5 +1,15 @@
 import Data.List
 import Data.Char
+import Data.Ord (comparing)
+import qualified Data.Set as Set
+
+primesToLimit :: Integer -> [Integer]
+primesToLimit m = 2 : sieve [3,5..m]
+  where
+    sieve (p:xs)
+      | p*p > m = p : xs
+      | True    = p : sieve [x | x <- xs, rem x p /= 0]
+
 
 problem22 = do
     inFile <- readFile "data/p22.dat"
@@ -16,6 +26,10 @@ problem25 = head [x+1 | x<-[1..], (length $ show $ fib $ x) == 1000 ]
           fib_base 0 = 1
           fib_base 1 = 1
           fib_base n = fib (n-2) + fib (n-1)
+
+problem27 = maximumBy (comparing snd) [(a*b, numPrimes a b) | a<-[-1000..1000], b<-(primesToLimit 1000)]
+    where numPrimes a b = length $ takeWhile (\x -> Set.member x primes) $ map (\x -> x^2 + a*x + b) [0..]
+          primes = Set.fromDistinctAscList $ primesToLimit 1000000
 
 problem29 = length $ nub $ [a^b| a<-[2..100], b<-[2..100] ]
 
