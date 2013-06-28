aa1eae07813b51a4b08b473ca4dd3ba9637a5e30
Simon Gibbons <simongibbons@gmail.com>
2013-06-28 18:23:58 +0100

    Added P46

--- problems41-50.hs
+++ problems41-50.hs
@@ -1,4 +1,12 @@
 import Data.Char
+import qualified Data.Set as Set
+
+primesToLimit :: Integer -> [Integer]
+primesToLimit m = 2 : sieve [3,5..m]
+  where
+    sieve (p:xs)
+      | p*p > m = p : xs
+      | True    = p : sieve [x | x <- xs, rem x p /= 0]
 
 problem42 = do
     fileIn <- readFile "data/p42.dat"
@@ -16,9 +24,19 @@ problem44 = head [x-y | x<-pents, y<-takeWhile (<x) pents, isPent (x+y), isPent 
 
           pents = [n*(3*n-1) `div` 2 | n<-[1..2500] ]
 
-problem_45 = head [x | x <- scanl (+) 1 [5,9..], x > 40755, isPent x]
+problem45 = head [x | x <- scanl (+) 1 [5,9..], x > 40755, isPent x]
     where isPent n =
             let (ai, af) = properFraction . sqrt $ 1 + 24 * (fromInteger n)
             in  (af == 0) && ai `mod` 6 == 5
+
+problem46 = head $ filter (isNotPrime) $ filter (isCounterExample) [3,5..]
+    where primes = Set.fromDistinctAscList $ primesToLimit 10000
+          isPrime x = (x `Set.member` primes)
+          isNotPrime x = (x `Set.notMember` primes )
+          isCounterExample x = isCounterExample' x 1
+          isCounterExample' x n
+                | (x - (2*n^2)) < 2     = True
+                | isPrime (x - (2*n^2)) = False
+                | otherwise             = isCounterExample' x (n+1)
 
 problem48 = ( sum $ [x^x `mod` (10^10) | x<-[1..1000] ] ) `mod` (10^10)
