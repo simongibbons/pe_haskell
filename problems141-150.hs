37d9114b0b993b045351d7e2c95a8f4a5574d841
Simon Gibbons <simongibbons@gmail.com>
2013-09-16 10:40:15 +0100

    Small Optimizeation of P145

--- problems141-150.hs
+++ problems141-150.hs
@@ -2,16 +2,18 @@ import Data.Char (digitToInt)
 
 --Only have contributions with odd nos of digits
 --Therefore only have to check upto 100 million.
---Brute Force runs in ~30s.
+--Brute Force runs in ~22s.
 problem145 = (length.(filter isRev)) [1..100000000]
     where rev :: Int -> Int
-          rev = (strToInt.reverse.show)
-              where strToInt :: String -> Int
-                    strToInt = fromDigits.(map digitToInt)
-
+          rev = (fromDigits.revDigs)
+              where
                     fromDigits :: [Int] -> Int
                     fromDigits = foldl addDigit 0
                         where addDigit num d = 10*num + d
+
+                    revDigs :: Int -> [Int]
+                    revDigs 0 = []
+                    revDigs x = (x `rem` 10):(revDigs (x `div` 10))
 
           allOdd :: Int -> Bool
           allOdd 0 = True
@@ -22,5 +24,3 @@ problem145 = (length.(filter isRev)) [1..100000000]
           isRev x | x `rem` 10 == 0 = False
                   | otherwise       = allOdd (x + (rev x) )
 
-main = do
-        print problem145
