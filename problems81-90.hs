79003bd468384065890477102a1809ccecd8c972
Simon Gibbons <simongibbons@gmail.com>
2014-01-03 21:47:51 +0000

    Done  P87

--- problems81-90.hs
+++ problems81-90.hs
@@ -1,4 +1,25 @@
 import qualified Data.Map as M
+import qualified Data.Set as Set
+
+primesToLimit :: Integer -> [Integer]
+primesToLimit m = 2 : sieve [3,5..m]
+  where
+    sieve (p:xs)
+      | p*p > m = p : xs
+      | True    = p : sieve [x | x <- xs, rem x p /= 0]
+
+-- Find the number of numbers expressible as the sum of a prime square, cube
+-- and fourth power below 50 million.
+problem87 = Set.size $ Set.fromList $
+            filter (<limit) [a+b+c | a<-prime4,
+                                     b<-(takeWhile (<limit-a) prime3),
+                                     c<-(takeWhile (<=limit-a-b) prime2)]
+  where
+    limit = 50000000
+    primes = primesToLimit 7071
+    prime2 = takeWhile (< limit) $ map (^2) primes
+    prime3 = takeWhile (< limit) $ map (^3) primes
+    prime4 = takeWhile (< limit) $ map (^4) primes
 
 problem89 = do
         contents <- readFile "data/p89.dat"
