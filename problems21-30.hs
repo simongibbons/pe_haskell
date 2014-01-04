01606fb821f2bf67a41e00cc3e9fe53db5c6a615
Simon Gibbons <simongibbons@gmail.com>
2014-01-04 10:49:26 +0000

    Done P26

--- problems21-30.hs
+++ problems21-30.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.Char
 import Data.Ord (comparing)
+import Data.Maybe
 import qualified Data.Set as Set
 
 primesToLimit :: Integer -> [Integer]
@@ -38,6 +39,19 @@ problem25 = head [x+1 | x<-[1..], (length $ show $ fib $ x) == 1000 ]
           fib_base 0 = 1
           fib_base 1 = 1
           fib_base n = fib (n-2) + fib (n-1)
+
+-- Find the largest reccuring cycle in the decimal expansion of 1/d
+-- for d < 1000
+problem26 = fst $ maximumBy (comparing snd) $
+            map (\x -> (x, cycleLength x)) [1..999]
+  where
+    cycleLength d = cycleLength' 1 d []
+
+    cycleLength' n d rems | m == 0    = 0
+                          | isJust i  = (fromJust i) + 1
+                          | otherwise = cycleLength' (10*m) d (m:rems)
+      where m = n `mod` d
+            i = findIndex (==m) rems --Slow because of the findIndex call
 
 problem27 = maximumBy (comparing snd) [(a*b, numPrimes a b) | a<-[-1000..1000], b<-(primesToLimit 1000)]
     where numPrimes a b = length $ takeWhile (\x -> Set.member x primes) $ map (\x -> x^2 + a*x + b) [0..]
