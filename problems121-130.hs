5c03134e20fcac98c06a3308d6b7a3fcc0d74bae
Simon Gibbons <simongibbons@gmail.com>
2014-03-17 21:44:30 +0000

    Done P123

--- problems121-130.hs
+++ problems121-130.hs
@@ -1,7 +1,10 @@
-import Math.NumberTheory.Primes (factorise)
+import Math.NumberTheory.Primes (factorise, primes)
 import Data.List (sortBy)
 import Data.Ord (comparing)
 
+problem123 = head $ dropWhile (\x -> fst x < 10^10) $
+             map (\x -> (sqRemainder x, fst x) ) $ zip [1..] primes
+  where sqRemainder (n, pn) = ((pn - 1)^n + (pn + 1)^n) `mod` pn^2
 
 problem124 = ( sortBy (comparing snd) rads ) !! 9999
   where rads = map (\x -> (x, radical x) ) [1..100000]
