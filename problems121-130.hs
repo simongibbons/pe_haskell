340380f03f71719c254e7707df0d874b1a7a99c4
Simon Gibbons <simongibbons@gmail.com>
2014-04-17 12:49:03 +0100

    added P121

--- problems121-130.hs
+++ problems121-130.hs
@@ -2,6 +2,29 @@ import Math.NumberTheory.Primes (factorise, primes)
 import Data.List (sortBy)
 import Data.Ord (comparing)
 
+--Could probably memoize the recursion for calculating ways but it seems
+--to run fast enough as it is.
+problem121 = prizeFund 15
+  where
+    prizeFund :: Integer -> Integer
+    prizeFund nTurns = (nOutcomes nTurns) `div` (nWins nTurns)
+
+    nWins :: Integer -> Integer
+    nWins nTurns = sum $ map (ways nTurns) $ [(nTurns `div` 2 + 1)..nTurns]
+
+    nOutcomes :: Integer -> Integer
+    nOutcomes nTurns = sum $ map (ways nTurns) $ [0..nTurns]
+
+    ways :: Integer -> Integer -> Integer
+    --Gives the number of ways of getting nblues
+    --from a game with n turns.
+    ways 1 0 = 1
+    ways 1 1 = 1
+    ways nTurns nBlues | nBlues > nTurns = 0
+                       | nBlues < 0      = 0
+                       | otherwise       = nTurns*(ways (nTurns - 1) nBlues ) + --gained a red
+                                           (ways (nTurns - 1) (nBlues - 1) )    --gained a blue
+
 problem123 = head $ dropWhile (\x -> fst x < 10^10) $
              map (\x -> (sqRemainder x, fst x) ) $ zip [1..] primes
   where sqRemainder (n, pn) = ((pn - 1)^n + (pn + 1)^n) `mod` pn^2
