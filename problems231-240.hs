33b7b9a0f12bb3812a7873689cb44570c1d6ebbd
Simon Gibbons <simongibbons@gmail.com>
2015-02-18 12:51:04 +0000

    Done P235

--- problems231-240.hs
+++ problems231-240.hs
@@ -11,3 +11,10 @@ problem231 = sumFactors 20000000 15000000
 
         primeExponent p = sum $ map summand $ takeWhile (\j -> p^j < n) [1..]
           where summand j = (n `div` p^j) - (r `div` p^j) - ( (n-r) `div` p^j )
+
+-- Mathematica Code....
+-- f[r_] := Sum[(900 - 3*k)*r^(k - 1), {k, 1, n}] /. n -> 5000
+-- FindRoot[f[r] == -600000000000, {r, 1.001}, WorkingPrecision -> 13]
+-- {r -> 1.002322108633}
+problem235 =
+    error "Done in Mathematica...... translate this to haskell at some point"
