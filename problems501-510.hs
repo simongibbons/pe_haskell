2f5c18bd7cfe300ec2c497b4171546588182e441
Simon Gibbons <simongibbons@gmail.com>
2015-04-30 12:30:18 +0100

    Slight readability improvement

--- problems501-510.hs
+++ problems501-510.hs
@@ -7,7 +7,8 @@ problem504 = length [1 | a <- coords, b <- coords, c <- coords, d <- coords,
         latPoints :: Int -> Int -> Int -> Int -> Int
         latPoints a b c d = (area2 - boundPoints) `div` 2 + 1
           where -- Twice the area of the shape
-                area2 = b*c + a*b + d*c + a*d
+                area2 = (a+c)*(b+d)
                 -- Number of points on the boundary
                 boundPoints = (gcd a d) + (gcd a b) + (gcd c b) + (gcd c d)
 
+main = print problem504
