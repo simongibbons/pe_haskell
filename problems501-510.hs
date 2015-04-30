b5532c31d4d3157ad6b1330c2955fd27f277ebbf
Simon Gibbons <simongibbons@gmail.com>
2015-04-30 12:58:31 +0100

    Precompute gcds

--- problems501-510.hs
+++ problems501-510.hs
@@ -1,3 +1,5 @@
+import qualified Data.Array as A
+
 problem504 = length [1 | a <- coords, b <- coords, c <- coords, d <- coords,
                          isSquare $ latPoints a b c d ]
   where coords = [1..100]
@@ -7,7 +9,10 @@ problem504 = length [1 | a <- coords, b <- coords, c <- coords, d <- coords,
           where -- Twice the area of the shape
                 area2 = (a+c)*(b+d)
                 -- Number of points on the boundary
-                boundPoints = (gcd a d) + (gcd a b) + (gcd c b) + (gcd c d)
+                boundPoints = (gcd' a d) + (gcd' a b) + (gcd' c b) + (gcd' c d)
 
         isSquare x = (floor . sqrt . fromIntegral $ x)^2 == x
+
+        gcdTable = A.listArray ((1,1),(100,100)) [gcd a b | a <- coords, b <- coords]
+        gcd' a b = gcdTable A.! (a,b)
 
